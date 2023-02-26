1be2496c212e06346e6b934997b027e8ef465e80
DaSc <das-sch@web.de>
2023-02-26 18:34:18 +0100

    add Events enum to EventHandler

--- src/plugin/EventHandler.py
+++ src/plugin/EventHandler.py
@@ -7,26 +7,33 @@ eh("event")               #Fire event "event" -> call all function in "event" ma
 eh.remove("event", funct) #remove function from "event" map
 """
 
+from enum import Enum
+
 
 class EventHandler:
+    class Events(Enum):
+        Health = 0
+        Item = 1
+        Money = 2
+
     def __init__(self):
-        self._events = {}
+        self._eventDict = {}
 
     def add(self, event, func):
-        if event not in self._events:
-            self._events[event] = {func}    #new map
+        if event not in self._eventDict:
+            self._eventDict[event] = {func}  # new map
         else:
-            self._events[event].append(func)
+            self._eventDict[event].append(func)
 
     def remove(self, event, func):
-        if event in self._events:
-            self._events[event].remove(func)
+        if event in self._eventDict:
+            self._eventDict[event].remove(func)
         else:
             print("EventHandler.remove(): event '", event, "' not in _events")
 
     def __call__(self, event, *args, **kwargs):
-        if event in self._events:
-            for func in self._events[event]:
+        if event in self._eventDict:
+            for func in self._eventDict[event]:
                 func(*args, **kwargs)
         else:
             print("EventHandler(): event '", event, "' not in _events")
