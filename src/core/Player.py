24a8d98528815a8e1ca0c03da8455ab2d39978fc
DaSc <das-sch@web.de>
2023-02-26 18:42:08 +0100

    Add Player health and functions

--- src/core/Player.py
+++ src/core/Player.py
@@ -6,6 +6,16 @@ class Player:
 
     def __init__(self, x, y, w, h):
         self.pos = GameBlock(x, y, w, h, BlockType.PLAYER)
+        self._health = 100
 
     def position(self):
         return [self.pos.x, self.pos.y, self.pos.width, self.pos.height]
+
+    def modify_health(self, modifier):
+        self._health += modifier
+        print ("Player Health: ", self._health)
+        if self._health <= 0:
+            self.death()
+
+    def death(self):
+        print ("X.X Du bist Tot X.X")
