46a9656e5c1a6b22683d5240b15956f11fe9fccb
FloBab73 <f.babel2000@gmail.com>
2023-04-06 18:53:39 +0200

    performance improvement through better algo

--- src/application/CollisionDetection.py
+++ src/application/CollisionDetection.py
@@ -14,39 +14,39 @@ class CollisionDetection:
         self.game_engine = game_engine
         self.event_handler = event_handler
 
-    def detect(self, subject, objects, border):
+    def detect(self, subject, objects, border_x, border_y):
         touch = set()
 
         for block in objects:
             if abs(block.x - subject.x) < 40 or abs(block.y - subject.y) < 40:
                 if self.game_engine.clipline(block,
-                                             subject.x + subject.width - 1 + border,
+                                             subject.x + subject.width - 1 + border_x,
                                              subject.y,
-                                             subject.x + subject.width - 1 + border,
+                                             subject.x + subject.width - 1 + border_x,
                                              subject.y + subject.height - 1):
                     touch.add(Direction.RIGHT)
                     subject.touch_block(block, Direction.RIGHT)
                 if self.game_engine.clipline(block,
                                              subject.x + subject.width - 1,
-                                             subject.y + subject.height - 1 + border,
+                                             subject.y + subject.height - 1 + border_y,
                                              subject.x,
-                                             subject.y + subject.height - 1 + border):
+                                             subject.y + subject.height - 1 + border_y):
                     touch.add(Direction.BOTTOM)
                     subject.touch_block(block, Direction.BOTTOM)
 
                 if self.game_engine.clipline(block,
-                                             subject.x - border,
+                                             subject.x - border_x,
                                              subject.y,
-                                             subject.x - border,
+                                             subject.x - border_y,
                                              subject.y + subject.height - 1):
                     touch.add(Direction.LEFT)
                     subject.touch_block(block, Direction.LEFT)
 
                 if self.game_engine.clipline(block,
                                              subject.x,
-                                             subject.y - border,
+                                             subject.y - border_y,
                                              subject.x + subject.width - 1,
-                                             subject.y - border):
+                                             subject.y - border_y):
                     touch.add(Direction.TOP)
                     subject.touch_block(block, Direction.TOP)
 
