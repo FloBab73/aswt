b7b7e9b1163c26da2d2a4e2e077fa5e530495103
FloBab73 <f.babel2000@gmail.com>
2023-05-06 17:01:26 +0200

    dead code in Enemy.py

--- src/domain/Enemy.py
+++ src/domain/Enemy.py
@@ -7,11 +7,3 @@ class Enemy(ActiveGameBlock):
     def __init__(self, x=0, y=0, width=10, height=10, max_speed=1):
         super().__init__(x, y, width, height, BlockType.ENEMY)
         self.max_speed = max_speed
-        self.x = x
-        self._startX = x
-        self.y = y
-        self._startY = y
-
-    def resetPos(self):
-        self.x = self._startX
-        self.y = self._startY
