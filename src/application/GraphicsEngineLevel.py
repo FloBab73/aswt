d100b13c25f82f2faadad4dccf1f289b71aa414b
DaSc <das-sch@web.de>
2023-05-06 18:49:34 +0200

    refactoring with dict for block type

--- src/application/GraphicsEngineLevel.py
+++ src/application/GraphicsEngineLevel.py
@@ -5,15 +5,21 @@ from src.domain.GraphicsEngine import GraphicsEngine
 class GraphicsEngineLevel(GraphicsEngine):
 
     def __init__(self, game_engine, level):
-        self.screen_width = 800
-        self.screen_height = 630
-        # TODO in überklasse auslagern
         self.gameEngine = game_engine
         self.screen = game_engine.init_display(self.screen_width, self.screen_height)
         self.level_keys = ""
         self.hud_y = self.screen_height - 30
         self.level = level
         self.level_keys = str(level.keys)
+        self.blockColors = {
+                        BlockType.ITEM_HEAL: (100, 255, 0),
+                        BlockType.ITEM_KEY: (0, 38, 200),
+                        BlockType.ITEM_VALUABLE: (0, 200, 200),
+                        BlockType.DOOR: (64, 64, 64),
+                        BlockType.PLAYER: (255, 255, 255),
+                        BlockType.ENEMY: (255, 0, 0),
+                        BlockType.WALL: (100, 50, 0),
+        }
 
     def draw(self):
         self.draw_level()
@@ -22,23 +28,9 @@ class GraphicsEngineLevel(GraphicsEngine):
 
     def draw_level(self):
         self.gameEngine.screen_fill(self.screen, [200, 150, 0])
-        # TODO refactoring with dict for block type
         for block in self.level.all_blocks:
-            match block.block_type:
-                case BlockType.ITEM_HEAL:
-                    self.gameEngine.draw_rect(self.screen, (100, 255, 0), block.position())
-                case BlockType.ITEM_KEY:
-                    self.gameEngine.draw_rect(self.screen, (0, 38, 200), block.position())
-                case BlockType.ITEM_VALUABLE:
-                    self.gameEngine.draw_rect(self.screen, (0, 200, 200), block.position())
-                case BlockType.DOOR:
-                    self.gameEngine.draw_rect(self.screen, (64, 64, 64), block.position())
-                case BlockType.PLAYER:
-                    self.gameEngine.draw_rect(self.screen, (255, 255, 255), block.position())
-                case BlockType.ENEMY:
-                    self.gameEngine.draw_rect(self.screen, (255, 0, 0), block.position())
-                case BlockType.WALL:
-                    self.gameEngine.draw_rect(self.screen, (100, 50, 0), block.position())
+            if block.block_type in self.blockColors:
+                self.gameEngine.draw_rect(self.screen, self.blockColors[block.block_type], block.position())
 
     def draw_hud(self):
         self.gameEngine.draw_rect(self.screen, [20, 20, 20], [0, self.hud_y, self.screen_width, 30])
