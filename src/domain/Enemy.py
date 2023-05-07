37022aeccff2a03c101ab65ac552eb5d060ca3a2
FloBab73 <f.babel2000@gmail.com>
2023-05-07 11:45:48 +0200

    reduce nesting in BlocksGenerator.py

--- src/domain/Enemy.py
+++ src/domain/Enemy.py
@@ -4,6 +4,6 @@ from src.domain.BlockType import BlockType
 
 class Enemy(ActiveGameBlock):
 
-    def __init__(self, x=0, y=0, width=10, height=10, color=(255, 0, 0), max_speed=1):
+    def __init__(self, x=0, y=0, width=11, height=11, color=(255, 0, 0), max_speed=1):
         super().__init__(x, y, width, height, color, BlockType.ENEMY)
         self.max_speed = max_speed
